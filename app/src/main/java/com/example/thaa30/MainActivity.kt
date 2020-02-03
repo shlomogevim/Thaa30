c2ea78b8281424eb4d5436f07ba1b6ce30a1a49f
Shlomo <shlomo.gevim@gmail.com>
2020-02-03 08:11:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/thaa30/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thaa30
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
